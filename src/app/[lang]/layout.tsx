5bd2e4b0bd32b669d09925786488a50f23034f17
mirafgan <mirefqan1999@gmail.com>
2025-08-24 01:00:56 +0400

    Refactor components and middleware for cleaner async props handling and consistent formatting.

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,43 +1,41 @@
-import type { Metadata } from "next"
-import type { ReactNode } from "react"
-import { Poppins } from "next/font/google"
+import type {Metadata} from "next"
+import type {ReactNode} from "react"
+import {Poppins} from "next/font/google"
 import "../globals.css"
-import { getDictionary } from "@/src/app/get-dictionary"
-import { i18n, type Locale } from "@/src/app/i18n-config"
+import {getDictionary} from "@/src/app/get-dictionary"
+import {i18n, type Locale} from "@/src/app/i18n-config"
 
-const inter = Poppins({ 
-  subsets: ["latin"], 
-  weight: ["400"]
+const inter = Poppins({
+    subsets: ["latin"],
+    weight: ["400"]
 })
 
 export async function generateStaticParams() {
-  return i18n.locales.map((locale) => ({ lang: locale }))
+    return i18n.locales.map((locale) => ({lang: locale}))
 }
 
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { lang: Locale }
-}): Promise<Metadata> {
-  const dictionary = await getDictionary(params.lang)
-  return {
-    title: dictionary.metadata.title,
-    description: dictionary.metadata.description,
-  }
+export async function generateMetadata({params}: { params: Promise<{ lang: Locale }> }): Promise<Metadata> {
+    const {lang} = await params;
+    const dictionary = await getDictionary(lang);
+    return {
+        title: dictionary.metadata.title,
+        description: dictionary.metadata.description,
+    }
 }
 
 type Props = {
-  children: ReactNode
-  params: { lang: Locale }
+    children: ReactNode
+    params: Promise<{ lang: Locale }>
 }
 
-export default function RootLayout({ children, params }: Props) {
-  return (
-    <html lang={params.lang} suppressHydrationWarning>
-      <body className={inter.className}>
+export default async function RootLayout({children, params}: Props) {
+    const {lang} = await params;
+    return (
+        <html lang={lang} suppressHydrationWarning>
+        <body className={inter.className}>
         {children}
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
